408af5c51ffc0cc653ee6e7f4605183965c544d4
renataalbq <renataalbq1@gmail.com>
2022-10-17 19:22:02 -0300

    componentes principais e estrutura da home"

---
+++ src/components/header/header.style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    title: {
+        textAlign: 'center',
+        fontSize: 20,
+        paddingVertical: 18,
+    }
+});
---
+++ src/components/search/search.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    search: {
+        marginTop: 20,
+        marginHorizontal: 18,
+        borderRadius: 10,
+        shadowColor: '#E4E4E4',
+        borderWidth: 1,
+        borderColor: '#E4E4E4',
+        borderStyle: 'solid',
+        backgroundColor: '#FAFAFA'
+    }
+});
