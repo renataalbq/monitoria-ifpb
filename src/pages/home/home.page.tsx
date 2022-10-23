ed5f3db9c44b8f0427c0e0cafde73f8d973ee045
renataalbq <renataalbq1@gmail.com>
2022-10-23 19:54:40 -0300

    add navigation

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,16 +1,16 @@
 import React from "react"
 import { Header } from "../../components/header/header.component"
-
 import { Divider } from 'react-native-paper';
 import { Button, View } from "react-native";
 
-export const HomePage = () => {
+export function HomePage({ navigation }) {
+    
     return (
         <View>
             <Header title={'Monitorando Monitor'} />
             <Divider />
             <View>
-                <Button title="Buscar monitores" />
+                <Button title="Buscar monitores" onPress={() => navigation.navigate('MonitoresPage')} />
                 <Button title="Buscar disciplinas" />
             </View>
         </View>
