d745266b5a932662adddbfa2d3b08117f0e94d23
renataalbq <renataalbq1@gmail.com>
2022-10-29 15:16:18 -0300

    bottom tab para navegaçãp

--- src/pages/disciplinas/disciplinas.style.ts
+++ src/pages/disciplinas/disciplinas.style.ts
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#FFF"
+    },
     list: {
         paddingVertical: 24,
     },
