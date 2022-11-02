9a8a951ac4552372d1d6548c5ee14a3232f4bfc0
renataalbq <renataalbq1@gmail.com>
2022-11-01 23:03:02 -0300

    add integration monitores

--- src/pages/monitores/monitores.page.tsx
+++ src/pages/monitores/monitores.page.tsx
@@ -1,24 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Header } from "../../components/header/header.component"
 import {Accordion} from "../../components/accordion/accordion.component"
 import {Search} from "../../components/search/search.component"
 import { Divider } from 'react-native-paper';
-import { View } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import {styles} from "./monitores.style"
 
 export const MonitoresPage = () => {
+    
+    const [monitores, setMonitores] = useState();
+    const [search, setSearch] = React.useState<string>('');
+
+    const handleSearch = (value: string) => {
+        setSearch(value);
+    };
+
+    useEffect(() => {
+        fetch(`https://fake-server-monitor.herokuapp.com/monitores?name_like=${search}`)
+        .then(response => response.json())
+        .then((data) => {
+          setMonitores(data)
+        })
+      }, []);
+
+      const renderItem = ({ item }: any) => (
+        <View>
+          <Accordion name={item.name}  />
+          <Divider />
+        </View>
+      );
+      
     return (
         <View style={styles.container}>
             <Header title={'Monitorando Monitor'} />
             <Divider />
             <Search placeholder={'Buscar monitor'} />
             <View style={styles.list}>
-                <Accordion name={"Aluno 1"} />
-                <Divider />
-                <Accordion name={"Aluno 2"} />
-                <Divider />
-                <Accordion name={"Aluno 3"} />
-                <Divider />
+                <FlatList 
+                    data={monitores}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+
+                />
             </View>
         </View>
     )
